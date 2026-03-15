2300ce508e06fedbb65022f6840836aa7b42ccdf
nguyenbabie1 <brokeniuem569@gmail.com>
2026-03-15 21:00:36 +0700

    Add files via upload

---
+++ src/sessoin6/demo/RandomStudentThread.java
@@ -0,0 +1,10 @@
+package sessoin6.demo;
+
+import java.util.Random;
+
+public class RandomStudentThread extends Thread {
+    String[] students = {"nguyen", "trang", "minh"};
+
+    Random random = new Random();
+
+}
